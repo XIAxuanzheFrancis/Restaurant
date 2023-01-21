de744192283aad65c18052189b5ad025ed6b20b4
xuanzhe <3058634081@qq.com>
2023-01-21 15:08:33 +0100

    tp1

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+// fonctions index.js
+
+function App() {
+    // La balise vide <> permet de s'assurer que les parenthèses contiennent une seule racine dans l'arbre HTML
+    return (
+    <>
+      <h1>Restauration Rapide </h1>
+      <p>Le meilleur endroit où manger sur les Cézeaux!</p>
+    </>
+
+    );
+}
+
+
+export default App;
