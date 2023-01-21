b1ccf95edca05250d9f2e9bad7f6f3e49aff92c8
xuanzhe <3058634081@qq.com>
2023-01-21 16:11:05 +0100

    tp1continue

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
+import logo from './logo.png';
+
 
 
 // fonctions index.js
@@ -8,6 +10,7 @@ function App() {
     // La balise vide <> permet de s'assurer que les parenthèses contiennent une seule racine dans l'arbre HTML
     return (
     <>
+      <img alt="logo" src={logo} />
       <h1>Restauration Rapide </h1>
       <p>Le meilleur endroit où manger sur les Cézeaux!</p>
     </>
